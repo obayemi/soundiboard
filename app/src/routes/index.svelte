9f18b5dc85bf138aa1588a8ac9e4bece42db16f1
obayemi <obayemi@ikjpz.com>
2022-03-05 03:30:17 +0100

    migrate files to hosting + db

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,54 +1,38 @@
 <script>
 	import { browser } from '$app/env';
 
-	const sounds = [
-		{ name: 'Arthur', file: '2. Arthur.mp3', volume: 1 },
-		{ name: 'Emotional damage', file: '3. Emotional Damage.mp3' },
-		{ name: 'Kais', file: '4. Kais.mp3', volume: 0.9 },
-		{ name: 'Je prout', file: '5. Je Prout.mp3' },
-		{ name: 'Leviosa', file: '6. Leviosa.mp3' },
-		{ name: 'Sound', file: '7. Sound.mp3', volume: 0.8 },
-		{ name: 'Ça a commencé ?', file: '8. Tout de suite la ca a commence.mp3', volume: 1 },
-		{ name: 'Toute de suite ?', file: '9. Tout de suite la.mp3', volume: 1 },
-		{ name: 'Whooa', file: '10. Whooa.mp3', volume: 1 },
-		{ name: 'Oula', file: 'oula.mp3', volume: 1 },
-		{ name: 'Yo glue', file: 'yo glue.mp3', volume: 1 },
-	];
-
 	let player;
-    let cheatCode = [];
+	let cheatCode = [];
+	export let sounds = [];
 
 	if (browser) {
 		document.onkeypress = function (event) {
 			const char = typeof event !== 'undefined' ? event.keyCode : event.which;
 
-                if (!isNaN(char)) {
-                        console.log(char, +char + 11);
-                        play(sounds[(+char + 11) % 10]);
-                    }
+			if (!isNaN(char)) {
+				play(sounds[(+char + 11) % 10]);
+			}
 
-                // queue structure: keep latest 5 key presses
-                if (cheatCode.length >= 4) {
-                        [, ...cheatCode] = cheatCode
-                    }
+			// queue structure: keep latest 5 key presses
+			if (cheatCode.length >= 4) {
+				[, ...cheatCode] = cheatCode;
+			}
 
-                cheatCode = [...cheatCode, String.fromCharCode(char)]
+			cheatCode = [...cheatCode, String.fromCharCode(char)];
 
-                        console.log(cheatCode)
-                // check cheat
-                if (cheatCode.toString() === 'p,p,s,p') {
-                        play({file:"ppsp.mp3"})
-                    }
-            };
+			// check cheat
+			if (cheatCode.toString() === 'p,p,s,p') {
+				play({ file_url: 'ppsp.mp3' });
+			}
+		};
 	}
 
 	function play(sound) {
-		console.log(sound);
 		if (player) {
 			player.pause();
 		}
-		player = new Audio(sound.file);
-		player.volume = sound.volume || 0.75;
+		player = new Audio(sound.file_url);
+		player.volume = sound.volume / 100;
 		player.play();
 	}
 </script>
@@ -92,7 +76,7 @@
 		font-weight: bold;
 		font-size: 1.5rem;
 		border-radius: 5px;
-		margin: 1rem;
+		margin: 0.3rem;
 		padding: 2rem;
 		height: 10rem;
 		width: 10rem;
