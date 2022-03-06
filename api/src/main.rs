6c7ef8efcbb64cae71b3c6fc4e77db3886ffb4fe
obayemi <obayemi@ikjpz.com>
2022-03-06 17:22:33 +0100

    allow CORS

--- api/src/main.rs
+++ api/src/main.rs
@@ -8,6 +8,28 @@ use dotenv::dotenv;
 use std::env;
 use rocket::serde::{Serialize, json::Json};
 use tracing::{info,warn};
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 
 #[derive(Serialize)]
@@ -77,6 +99,7 @@ async fn main() {
             sounds_list,
             sounds_detail,
             ])
+        .attach(CORS)
         .manage(pool)
         .launch()
         .await
